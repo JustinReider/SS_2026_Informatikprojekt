7d6c62ad6ebc812e3421171acff69a59a44490c2
1Zane0 <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@zaneprojects@proton.me>
2026-05-27 19:19:31 +0200

    Remove files that should be gitignored

--- Assets/Scripts/PlayerPersist.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class PlayerPersist : MonoBehaviour {
-
-    void Start() {
-        DontDestroyOnLoad(gameObject); // = Player
-        DontDestroyOnLoad(Camera.main.gameObject); // = Kamera
-    }
-}
