a5210a127926670ea9f7580f020ce7cd2392cf8c
Jus10eR <84348179+Jus10eR@users.noreply.github.com>
2026-06-23 21:31:22 +0200

    markt personen und quick fixes

--- Assets/Scripts/PickupHandler.cs
+++ Assets/Scripts/PickupHandler.cs
@@ -5,8 +5,7 @@ public class PickupHandler : MonoBehaviour
 {
     public Transform rightHand;
     public Transform leftHand;
-
-    private Dictionary<string, Vector3> originalScales = new();
+    private Dictionary<string, Vector3> originalWorldScales = new();
 
     public void PickUpRight(string objectName) => Pickup(objectName, rightHand);
     public void PickUpLeft(string objectName)  => Pickup(objectName, leftHand);
@@ -17,10 +16,13 @@ public class PickupHandler : MonoBehaviour
         GameObject obj = GameObject.Find(objectName);
         if (obj == null) return;
 
-        originalScales[objectName] = obj.transform.localScale;
+        // World-Scale VOR dem Parenting speichern
+        originalWorldScales[objectName] = obj.transform.lossyScale;
 
-        obj.transform.SetParent(hand, true); // worldPositionStays = true
-        obj.transform.localScale = originalScales[objectName];
+        obj.transform.SetParent(hand, true);
+
+        // World-Scale nach dem Parenting wiederherstellen
+        ApplyWorldScale(obj.transform, originalWorldScales[objectName]);
     }
 
     void Drop(string objectName)
@@ -28,17 +30,34 @@ public class PickupHandler : MonoBehaviour
         GameObject obj = GameObject.Find(objectName);
         if (obj == null) return;
 
-        Vector3 currentWorldPos = obj.transform.position;
-        Quaternion currentWorldRot = obj.transform.rotation;
-        Vector3 scale = originalScales.ContainsKey(objectName) 
-            ? originalScales[objectName] 
-            : obj.transform.localScale;
+        Vector3 worldPos = obj.transform.position;
+        Quaternion worldRot = obj.transform.rotation;
+        Vector3 worldScale = originalWorldScales.ContainsKey(objectName)
+            ? originalWorldScales[objectName]
+            : obj.transform.lossyScale;
 
         obj.transform.SetParent(null);
-        obj.transform.position   = currentWorldPos;
-        obj.transform.rotation   = currentWorldRot;
-        obj.transform.localScale = scale;
+        obj.transform.position = worldPos;
+        obj.transform.rotation = worldRot;
+        ApplyWorldScale(obj.transform, worldScale);
 
-        originalScales.Remove(objectName);
+        originalWorldScales.Remove(objectName);
+    }
+
+    void ApplyWorldScale(Transform t, Vector3 targetWorldScale)
+    {
+        // localScale berechnen der die gewünschte World-Scale ergibt
+        if (t.parent == null)
+        {
+            t.localScale = targetWorldScale;
+            return;
+        }
+
+        Vector3 parentScale = t.parent.lossyScale;
+        t.localScale = new Vector3(
+            parentScale.x != 0 ? targetWorldScale.x / parentScale.x : targetWorldScale.x,
+            parentScale.y != 0 ? targetWorldScale.y / parentScale.y : targetWorldScale.y,
+            parentScale.z != 0 ? targetWorldScale.z / parentScale.z : targetWorldScale.z
+        );
     }
 }
