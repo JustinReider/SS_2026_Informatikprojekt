fd6e2cf7ce5b1b1b6cbbe28a46e28da24c218c1d
1Zane0 <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@zaneprojects@proton.me>
2026-06-24 23:16:18 +0200

    SlaveIntro

--- Assets/Market/Slave_Route/MausUmschauen.cs
+++ Assets/Market/Slave_Route/MausUmschauen.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 
 public class MausUmschauen : MonoBehaviour
 {
+    public Transform sklave_body; // ← Der Sklave Body (Parent)
     public float mouseSensitivity = 2f;
     private float rotationX = 0f;
 
     void Update()
     {
-        // Maus Input
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        // Vertikal schauen (oben/unten)
+        // Vertikal (Kamera oben/unten)
         rotationX -= mouseY;
         rotationX = Mathf.Clamp(rotationX, -90f, 90f);
-
-        // Anwenden
         transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-        // Parent (Sklave Body)
+
+        // Horizontal (Body links/rechts)
+        sklave_body.Rotate(Vector3.up * mouseX);
     }
 }
